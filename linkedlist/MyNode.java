ee44979185ba0c9cbc303358539fa9ca60b0e108
sreeja99 <srijagodishala@gmail.com>
2020-10-10 17:21:45 +0530

    Refactored:Created a Linked List and Tested It

--- linkedlist/MyNode.java
+++ linkedlist/MyNode.java
@@ -1,5 +1,25 @@
 package com.capgemini.linkedlist;
 
-public class MyNode {
+public class MyNode<K> {
+
+	private K key;
+	private MyNode next;
+
+	public MyNode(K key) {
+		this.key=key;
+		this.next=null;
+	}
+	public K getKey() {
+		return key;
+	}
+	public MyNode getNext() {
+		return next;
+	}
+	public void setKey(K key) {
+		this.key = key;
+	}
+	public void setNext(MyNode next) {
+		this.next=next;
+	}
 
 }
---
+++ linkedlist/MyNodeTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyNodeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
