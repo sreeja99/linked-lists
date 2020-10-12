a3b1d6ee321e2e818232b6b00a4c3f381d64bc59
sreeja99 <srijagodishala@gmail.com>
2020-10-12 16:26:05 +0530

    Refactored:Created A Method To Traverse The Tree And Check For 63

--- linkedlist/MyBinaryTreeTest.java
+++ linkedlist/MyBinaryTreeTest.java
@@ -36,4 +36,23 @@ class MyBinaryTreeTest {
 		int size = myBinaryTree.getSize();
 		assertEquals(13, size);
 	}
+	@Test
+	public void searchForParticularKey() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		MyBinaryNode<Integer> reqNum =myBinaryTree.search(63);
+		assertEquals(63,(int)reqNum.key);
+	}
 }
